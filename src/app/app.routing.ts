96ba15388e5b5a84838fe378e4e4f32474863249
Isuru Maduranga <isurumaduranga.16@cse.mrt.ac.lk>
2020-01-31 19:06:31 +0530

    some changes

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -11,7 +11,8 @@ const routes: Routes =[
     path: '',
     redirectTo: 'dashboard',
     pathMatch: 'full',
-  }, {
+  }, 
+  {
     path: '',
     component: AdminLayoutComponent,
     children: [
